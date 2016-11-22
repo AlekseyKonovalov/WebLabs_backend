9438538eae9b34e9697c869bbb41dad3ba4ed01d
AlekseyKonovalov <konov.95@mail.ru>
2016-11-22 13:33:51 +0500

    2

--- js/app.js
+++ js/app.js
@@ -42,25 +42,71 @@ app.config(function($routeProvider) {
             templateUrl:"assets/directives/pokemon.html",
             replace: true,
             restrict: 'E',
-            controller:  function($scope){
-                $scope.pokPossition={'X':0,'Y':0};
-                var tictac, tic=0;
-                $scope.start=function(){
+            controller:  function($scope, $interval){
+                $scope.pokPos={'X':5,'Y':1};
+                $scope.pokWidth=250;
+                $scope.pokheight=180;
 
-                    tictac=$interval(function(){
-                        tic++;
-                        $scope.pokPossition.X=50*Math.sin(tic/50);
-                        $scope.pokPossition.Y=20*Math.cos(tic/20);
-                    },50);
-                };
-                $scope.stop=function(){
-                    $interval.cancel(tictac);
-                };
+                var tictac, tic=0;
+                tictac=$interval(function(){
+                    tic++;
+                    $scope.pokPos.X=50*Math.sin(tic/50);
+                    $scope.pokPos.Y=30*Math.cos(tic/40);
+
+                },50);
+
+
+
             }
         }
     })
 
+    .controller("gameController", function ($scope, $interval, scoreFac, levelFac) {
+        $scope.score=scoreFac.getScore();
+        $scope.level=levelFac.getLevel();
+        $scope.time=0;
 
+        var tictac=$interval(function(){
+
+            $scope.time++
+
+            scoreFac.setScore($scope.score-10);
+
+            $scope.score=scoreFac.getScore();
+
+        },300);
+
+
+    })
+
+    .factory("scoreFac", function() {
+        var score = 1000;
+        function setScore(value) {
+            score=value;
+        };
+        function getScore() {
+            return score
+        };
+        return {
+            getScore: getScore,
+            setScore:setScore
+        };
+    })
+
+
+    .factory("levelFac", function() {
+        var level = 2;
+        function setLevel(value) {
+            level=value;
+        };
+        function getLevel() {
+            return level
+        };
+        return {
+            getLevel: getLevel,
+            setLevel: setLevel
+        };
+    })
 
 ;
 
